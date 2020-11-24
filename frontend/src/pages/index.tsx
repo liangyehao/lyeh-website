f46be2346f2f32e76e0fca8a1c6e2cbe3e4c8199
liangyehao <1094311509@qq.com>
2020-11-24 22:17:36 +0800

    访问首页弹出notification

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import { Avatar,Card } from 'antd';
+import { Avatar,notification } from 'antd';
 import styles from './index.less';
 
 import logo from "../images/logo.jpg"
 
+const openNotification = () => {
+  notification.info({
+    message: '欢迎来到梁业浩的个人网站',
+    placement: 'bottomRight',
+    description:
+      '在这里，我会分享我关注的技术、学习的心得、遇到的问题O(∩_∩)O',
+  });
+};
+
 export default () => {
+  openNotification();
   return (
     <div>
       <h1 className={styles.title}>
