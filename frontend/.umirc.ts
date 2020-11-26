e2bdc6443de5f3f16d266ec79a38384707e562a6
liangyehao <1094311509@qq.com>
2020-11-27 00:40:53 +0800

    history: {     type: 'hash',   }  解决打包后路由404问题，（地址栏会自动加上#)

--- frontend/.umirc.ts
+++ frontend/.umirc.ts
@@ -3,5 +3,15 @@ import { defineConfig } from 'umi';
 export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
+  },
+  history: {
+    type: 'hash',
+  },
+  proxy:{
+    '/tuling': {
+      target: 'http://openapi.tuling123.com', //  需修改为真实后端服务地址
+      changeOrigin: true,
+      pathRewrite: { "^/tuling" : "" }
+    }
   }
 });
