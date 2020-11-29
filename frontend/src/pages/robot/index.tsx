8b0b346bd72bc0678c2b3a7e7fe2d2e903fc1635
liangyehao@huawei <1094311509@qq.com>
2020-11-29 18:42:46 +0800

    1.useState hook的使用 2.ts类型限制 3.利用函数式组件重构robot页面

--- frontend/src/pages/robot/index.tsx
+++ frontend/src/pages/robot/index.tsx
@@ -1,82 +1,67 @@
-import React, { Component, useState } from 'react';
-import { Input,Alert,Divider,Button } from "antd"
+import React, { useState } from 'react';
+import { Input, Alert, Divider, Button } from 'antd';
 
-const {Search} = Input;
+const { Search } = Input;
 
 import request from 'umi-request';
 
-import {goTo} from '@/common/utils';
 import config from '@/config/config';
 
 
-class Index extends Component {
-
-  constructor(props: {}) {
-    super(props);
-    this.state={
-      text:'随便问我点什么吧...'
-    };
-  }
-
-  componentDidMount() {
-    // this.chat("高州天气");
-    console.log(config.backendUrl)
-  }
-
-  chat = (msg:string) => {
-    const that = this;
+const robot = (props: any) => {
+  const [text,setText] = useState('随便问点什么吧...');
+  function chat(value: string): void {
     request(config.backendUrl, {
       method: 'post',
       data: {
-        "reqType":0,
-        "perception": {
-          "inputText": {
-            "text": msg
-          }
+        'reqType': 0,
+        'perception': {
+          'inputText': {
+            'text': value,
+          },
         },
-        "userInfo": {
-          "apiKey": "c1fd05473b5b4dea86a315e041e9b732",
-          "userId": "599310"
-        }
+        'userInfo': {
+          'apiKey': 'c1fd05473b5b4dea86a315e041e9b732',
+          'userId': '599310',
+        },
       },
     })
       .then(function(response) {
-        console.log(response);
-        const {results} = response;
-        const {values:{text}} = results[0];
-        that.setState({text})
+        // console.log(response);
+        const { results } = response;
+        const { values: { text } } = results[0];
+        setText(text);
       })
       .catch(function(error) {
+        setText("出现异常啦，请联系网站开发者"+error)
         console.log(error);
       });
-  };
-
-  render(){
-    return (
-      <div style={{marginTop:'20vh'}}>
-        <Divider>聊天机器人，欢迎来撩~</Divider>
-        <Search
-          placeholder="例如：广州天气"
-          // allowClear
-          onSearch={this.chat}
-          style={{ width: 300, margin: '10px 10px' }}
-          enterButton
-        />
-        <div style={{textAlign:'center'}}>
-          <Alert
-            message={this.state.text}
-            type="info"
-            // closable={true}
-            style={{ width: 300,margin:'auto' }}
-            onClose={close}
-            // closable={true}
-            showIcon
-          />
-        </div>
-      </div>
-    );
   }
 
-}
 
-export default Index;
+  return (
+    <div style={{ marginTop: '20vh' }}>
+      <Divider>聊天机器人，欢迎来撩~</Divider>
+      <Search
+        placeholder="例如：广州天气"
+        // allowClear
+        onSearch={(value) => {
+          chat(value);
+        }}
+        style={{ width: 300, margin: '10px 10px' }}
+        enterButton
+      />
+      <div style={{ textAlign: 'center' }}>
+        <Alert
+          message={text}
+          type="info"
+          // closable={true}
+          style={{ width: 300, margin: 'auto' }}
+          showIcon
+        />
+      </div>
+    </div>
+  );
+};
+
+export default robot;
